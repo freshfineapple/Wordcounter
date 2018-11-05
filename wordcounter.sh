031802776ea6c66074c60c1d0689ff879daad317
freshfineapple <pbeissinger@gmail.com>
2018-11-04 22:45:59 -0800

    Initial impl of script. Only counts .docx files. Added sample docx file with twelve words

--- wordcounter.sh
+++ wordcounter.sh
@@ -1,2 +1,10 @@
 #!/usr/bin/env bash
-echo "Hello world!"
+# Counts all the words in all the .docx files in the current directory
+# TODO: take a dir parameter, defaulting to current dir if none passed in.
+# ASPIRATIONAL TODO: Can I store the .txt versions in memory instead of writing to disk? (better perf this way)
+
+mkdir -p wordcount-temp
+cp *.docx wordcount-temp
+textutil -convert txt wordcount-temp/*.docx
+cat wordcount-temp/*.txt | wc -w
+rm -r wordcount-temp
